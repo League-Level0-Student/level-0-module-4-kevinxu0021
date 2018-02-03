25d4864883720bc499d1f1752a272d96e9872082
league <league@wts5.attlocal.net>
2018-02-03 12:56:43 -0800

    Don't Forget The Lyrics and Pi Aloud.

--- src/PiAloud.java
+++ src/PiAloud.java
@@ -8,20 +8,32 @@ import java.util.Scanner;
 public class PiAloud {
 
 	// 1. Make a main method and make sure your program can run
+public static void main(String[] args) {
+	
 
 	// 2. Make a String variable to hold the value of Pi. You could use http://www.piday.org/million/ for the value.
-
-	// 3. Print out some digits of Pi. The first value is "pi.charAt(0)", the second is "pi.charAt(1)"
+String Pi = "3.141592653589793238462643383279502";
 
 	// 4. Print ALL the digits of of Pi (hint: use a loop)
+for(int i = 0; i < Pi.length(); i++) {
+	if(getInputFromUser() == Pi.charAt(i)) {
+		System.out.println("Correct!");
+	}else {
+		System.out.println("Wrong!");
+	}
+	System.out.println(Pi.charAt(i));
+	speak(Pi.charAt(i));
+}
 
 	// 5. Use the speak() method to speak all the digits of Pi.
 
 	// [ADVANCED]
 	// *6. Get a character from the user using the getInputFromUser() method
+
 	// *7. Compare the users' char to the next digit of Pi
 	// *8. If they are correct, print out "correct". If they are not, print "incorrect" to System.err.println
 
+}
 	static void speak(char characterToSpeak) {
 		try {
 			Runtime.getRuntime().exec("say " + characterToSpeak).waitFor();
@@ -42,7 +54,9 @@ public class PiAloud {
 	static char getInputFromUser() {
 		Scanner scanner = new Scanner(System.in);
 		return scanner.next().toCharArray()[0];
+	
 	}
+
 
 }
 
