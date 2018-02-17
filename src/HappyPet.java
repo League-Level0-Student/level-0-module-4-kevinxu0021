282503bcfc9fbe7113abea3776eee0868509af1a
league <league@wts21.attlocal.net>
2018-02-17 12:57:26 -0800

    happy Pet and Sleepy Head

--- src/HappyPet.java
+++ src/HappyPet.java
@@ -8,26 +8,58 @@ import javax.swing.JOptionPane;
 public class HappyPet {
 	// 2. Create a happinessLevel variable to store the pet's happiness number.
 	//    Initialize to zero.
-
+static int happinessLevel = 0;
+static String userPet;
 	public static void main(String[] args) {
 		// 1. Ask the user what kind of pet they want to buy, and store in variable
-
+ userPet = JOptionPane.showInputDialog(null, "What pet do you want to buy?", "Happy Pet", JOptionPane.INFORMATION_MESSAGE);
 		// 7. REPEAT steps 3 - 6 enough times to make your pet happy!
+		for (;happinessLevel < 25;) {
+			
 		
 			// 3. Use showOptionDialog to ask the user what they want to do to make their pet happy
-			//    (eg: cuddle, food, water, take a walk, groom, clean up poop).
+			//    (eg: cuddle, food, water, take a walk, groom, clean up poop).\
+				
 			//    Make sure to customize the title and question too.
-			int task = JOptionPane.showOptionDialog(null, "Question", "Title", 0, JOptionPane.INFORMATION_MESSAGE, null,
-					new String[] { "Button1", "Button2", "Button3" }, null);
-
+			int task = JOptionPane.showOptionDialog(null, "What do you want to do to make your pet happy?", "Happy Pet", 0, JOptionPane.INFORMATION_MESSAGE, null,
+					new String[] { "Hit Your Pet","Clean Up Poop", "Food", "Water", "Take a Walk" }, null);
 			// 5. Use user input to call the appropriate method created in step 4.
-
+if(task == 4) {
+	takeAWalk();
+}else if(task == 3) {
+	Water();
+}else if(task == 2) {
+	Food();
+}else if(task == 1) {
+	cleanUpPoop();
+}else if(task == 0) {
+	hitYourPet();
+}
 			// 6. If you determine the happiness level is large enough, tell the
 			//    user that he loves his pet and use break; to exit for loop.
-
+if(happinessLevel == 25){
+JOptionPane.showMessageDialog(null, "You love this pet!");
+}
+		}
 	}
 
 	// 4. Create methods to handle each of your user selections.
 	//    Each method should create a pop-up with the pet's response (eg. cat might purr when pet), 
 	//    and INCREMENT the pet's happiness Level.
+	static void cleanUpPoop() {
+		JOptionPane.showMessageDialog(null, userPet + " might go near you when you clean up its poop.");
+		happinessLevel +=3;
+	}static void Food() {
+		JOptionPane.showMessageDialog(null, userPet + " might go with you when you give it food.");
+		happinessLevel +=5;
+	}static void Water() {
+		JOptionPane.showMessageDialog(null, userPet + " might do nothing when you give it water.");
+		happinessLevel +=1;
+	}static void takeAWalk() {
+		JOptionPane.showMessageDialog(null, userPet + " might like you when you take a walk with it.");
+		happinessLevel +=4;
+	}static void hitYourPet() {
+		JOptionPane.showMessageDialog(null, userPet + " might hat you if you hit it.");
+		happinessLevel -=5;
+	}
 }
--- src/SleepyHead.java
+++ src/SleepyHead.java
@@ -16,13 +16,33 @@ public class SleepyHead {
          * JOptionPane.showConfirmDialog(null, "is it a weekday?", "",
          * JOptionPane.YES_NO_OPTION);
          */
-    
-
+        int answer = JOptionPane.showConfirmDialog(null, "Is it a weekday?", "Sleepy Head", JOptionPane.YES_NO_OPTION);
+        int Answer = JOptionPane.showConfirmDialog(null, "Is it a vacation?", "Sleepy Head", JOptionPane.YES_NO_OPTION);
+   
+if(answer == 1){
+	isWeekday = false;
+}else{
+	isWeekday = true;
+}
+if(Answer == 1) {
+	isVacation = false;
+}else{
+	isVacation = true;
+}
 
         /*
          * Print “sleep in” if it is a vacation or a weekend. If it’s a weekday,
          * print “get up lazybones!” If it is a weekday, and we are on vacation,
          * print “sleep in”.
          */
+if(isWeekday == true && isVacation == true) {
+	JOptionPane.showMessageDialog(null, "Sleep in.");
+}else if(isWeekday == false && isVacation == true) {
+	JOptionPane.showMessageDialog(null, "Sleep in.");
+}else if(isWeekday == true && isVacation == false){
+	JOptionPane.showMessageDialog(null, "Get up lazybones!");
+}else if(isWeekday == false) {
+	JOptionPane.showMessageDialog(null, "Sleep in.");
+}
     }
 }
