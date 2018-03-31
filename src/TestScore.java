de6120a2dd8d7b2ccee0ba5bce2073385452f79c
league <league@wts20.attlocal.net>
2018-03-31 11:59:09 -0700

    Test Score

--- src/TestScore.java
+++ src/TestScore.java
@@ -4,5 +4,36 @@ public class TestScore {
 public static void main(String[] args) {
 
 	int score = JOptionPane.showOptionDialog(null, "What score did you get on your test?", "Test Score", 0, JOptionPane.INFORMATION_MESSAGE, null, new String[] {"100%", "<100% but >80%", "<80% but >60%", "<60%"}, null);
-}				
+System.out.println(score);
+	if(score == 0) {
+		JOptionPane.showMessageDialog(null, "Wow! You must have studied really hard for that test!");
+}else if(score == 1) {
+	JOptionPane.showMessageDialog(null, "You did study for the test.");
+}else if(score == 2) {
+	JOptionPane.showMessageDialog(null, "You did not study for the test.");
+}else {
+	JOptionPane.showMessageDialog(null, "You do not know anything!");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
 }
